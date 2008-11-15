8eacbefdf6dadc035390c56cc79ad6dc5e5a5260
Alan Gutierrez <alan@blogometer.com>
2008-11-15 22:40:37 +0000

    - Reverting deleted tests.
    git-svn-id: http://blogometer.com/svn/open/trunk/synapse@2296 3df9ba58-24f1-0310-ade5-ba95bd924a04

---
+++ src/test/java/org/curlybraces/synapse/ServletTest.java
@@ -0,0 +1,46 @@
+package org.curlybraces.synapse;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.servlet.ServletHandler;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ServletTest
+{
+    Server server = new Server();
+
+    @BeforeMethod public void startJetty() throws Exception
+    {
+        SocketConnector connector = new SocketConnector();
+        // Set some timeout options to make debugging easier.
+        connector.setMaxIdleTime(1000 * 60 * 60);
+        connector.setSoLingerTime(-1);
+        connector.setPort(8888);
+        server.setConnectors(new Connector[] { connector });
+
+
+        ServletHandler handler = new ServletHandler();
+       
+        handler.addServletWithMapping(SynapseServlet.class, "/synapse");
+        server.addHandler(handler);
+        server.start();
+    }
+    
+    @Test public void test() throws Exception 
+    {
+        Locator locator = new Locator("localhost", 8888);
+        Synapse synapse = new Synapse(new Echo("Hello, World!"));
+        locator.sendCommand(synapse);
+        
+        Thread.sleep(2000);
+    }
+    
+    @AfterMethod public void stopJetty() throws Exception
+    {
+        server.stop();
+        server.join();
+    }
+}
---
+++ src/test/java/org/curlybraces/synapse/TokenizerTest.java
@@ -0,0 +1,35 @@
+package org.curlybraces.synapse;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+public class TokenizerTest
+{
+    private final static String ID = "7b2fb945-c913-48aa-ad25-346e27c2064b";
+
+    private final static Date DATE = new Date(1226453280480L);
+
+    @Test public void tokenize()
+    {
+        Missive missive = new Missive();
+        
+        missive.setId(ID);
+        missive.setDate(DATE);
+        missive.setMessage("This is a test.");
+        missive.setPersonId(ID);
+        
+        Tokenizer tokenizer = new Tokenizer();
+        List<Term> terms = tokenizer.tokenize(missive);
+        
+        assertEquals(terms.size(), 5);
+        assertEquals(terms.get(0).getWord(), ID.toString());
+        assertEquals(terms.get(1).getWord(), "this");
+        assertEquals(terms.get(2).getWord(), "is");
+        assertEquals(terms.get(3).getWord(), "a");
+        assertEquals(terms.get(4).getWord(), "test");
+    }
+}
