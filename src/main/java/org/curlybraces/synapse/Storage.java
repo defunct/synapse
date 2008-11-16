1ec9c9aca913d62b2cceca1bf6414429884e2574
Alan Gutierrez <alan@blogometer.com>
2008-11-16 05:22:48 -0600

    - Created a Storage class to manage a set of Bins, each bin responsible   for a range of objects keyed on a UUID.

---
+++ src/main/java/org/curlybraces/synapse/Storage.java
@@ -0,0 +1,29 @@
+package org.curlybraces.synapse;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.UUID;
+
+public class Storage<T>
+{
+    private final SortedMap<UUID, Bin<T>> mapOfBins;
+    
+    public Storage()
+    {
+        this.mapOfBins = new TreeMap<UUID, Bin<T>>(new Reverse<UUID>());
+    }
+    
+    public T get(UUID id)
+    {
+        SortedMap<UUID, Bin<T>> tail = mapOfBins.tailMap(id);
+        if (tail.size() != 0)
+        {
+            Bin<T> bin = tail.get(tail.firstKey());
+            if (id.compareTo(bin.getNext()) < 0)
+            {
+                return bin.get(id);
+            }
+        }
+        return null;
+    }
+}
